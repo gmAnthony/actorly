bedb5a511eee9931932871fa794b80b1fe24e963
Anthony Miller <gm.anthony@gmail.com>
2022-02-22 22:01:10 -0600

    bring in sass, basic layout

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,22 @@
+import './App.scss';
+
+const title = 'Actorly Davis';
+
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <header className="App-header">
+        {title}
+      </header>
+      <main className="main-container" role="main">
+        <h2>See which movies actors have in common.</h2>
+        <ul className="steps-list">
+          <li>Step 1: Search for actors</li>
+          <li>Step 2: That's it.</li>
+        </ul>
+        <form />
+      </main>
+    </div>
   );
 }
 
