eb8241df38607c3669c83d8b8561bbd9ab240b38
Anthony Miller <gm.anthony@gmail.com>
2022-02-24 22:20:18 -0600

    bring in force graph, basic settings

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,19 @@
 import './App.scss';
 import { ActorSearch } from './components/ActorSearch';
+import { ActorGraph } from './components/ActorGraph';
 import Button from '@mui/material/Button';
 
 const title = 'Actorly Davis';
+const N = 300;
+const gData = {
+  nodes: [...Array(N).keys()].map(i => ({ id: i })),
+  links: [...Array(N).keys()]
+    .filter(id => id)
+    .map(id => ({
+      source: id,
+      target: Math.round(Math.random() * (id-1))
+    }))
+  }
 
 function App() {
   return (
@@ -22,6 +33,8 @@ function App() {
             Actorize
           </Button>
         </form>
+        <div id='actor-graph' />
+        <ActorGraph data={gData} />
       </main>
     </div>
   );
